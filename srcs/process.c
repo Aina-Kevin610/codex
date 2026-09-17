707b6fa8de668903d871054102da8e5004f042f6
Aina Kevin Randrianambinintsoa <airandri@student.42antananarivo.mg>
2026-09-17 14:42:40 +0300

    feat: add 42 header

--- srcs/process.c
+++ srcs/process.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: airandri <airandri@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/17 14:32:40 by airandri          #+#    #+#             */
+/*   Updated: 2026/09/17 14:32:41 by airandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../headers/codexion.h"
  
 int	act(t_coder *coder)
@@ -6,16 +18,13 @@ int	act(t_coder *coder)
 
 	signal = 1;
 	coder->step = 1;
-	if (coder->compile_done <= coder->all->arguments->nb_compiles)
-	{
-		pthread_mutex_lock(&coder->dongle_left->dongle_lock);
-		request(coder, coder->dongle_left);
-		pthread_mutex_unlock(&coder->dongle_left->dongle_lock);
-		pthread_mutex_lock(&coder->dongle_right->dongle_lock);
-		request(coder, coder->dongle_right);
-		pthread_mutex_unlock(&coder->dongle_right->dongle_lock);
-		take_dongle(coder);
-	}
+	pthread_mutex_lock(&coder->dongle_left->dongle_lock);
+	request(coder, coder->dongle_left);
+	pthread_mutex_unlock(&coder->dongle_left->dongle_lock);
+	pthread_mutex_lock(&coder->dongle_right->dongle_lock);
+	request(coder, coder->dongle_right);
+	pthread_mutex_unlock(&coder->dongle_right->dongle_lock);
+	take_dongle(coder);
     signal *= compile(coder);
 	release_dongle(coder);
     signal *= debug(coder);
